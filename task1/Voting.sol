e0fed9ef099eb815ea7fe81fc0f21403fc1b980a
jchaw2023 <jchaw2006@gmail.com>
2025-11-27 02:47:27 +0800

    改进投票计数 不使用数组参照去比较某个候选人是否被投过票

--- task1/Voting.sol
+++ task1/Voting.sol
@@ -2,8 +2,13 @@
 pragma solidity ^0.8.0;
 
 contract Voting {
+
+    struct VotedCount {
+       bool exists;
+       uint count;
+    }
     // 候选人得票记录
-    mapping(string => uint) public votingCountMap;
+    mapping(string => VotedCount) public votingCountMap;
 
     // 投票人记录
     mapping(address => bool) public votingRecord;
@@ -20,26 +25,15 @@ contract Voting {
      */
     function vote(string calldata name) public {
         address currentAddr = msg.sender; // 投票人地址
-
         require(!votingRecord[currentAddr], "Error: Double voting!");
         votingRecord[currentAddr] = true;
         votingAddrs.push(currentAddr);
-
-        votingCountMap[name] += 1; // 候选人投票数量+1
-
-        // 记录候选人列表，去重
-        uint length = votedNames.length;
-        bool has = false;
-
-        for (uint i = 0; i < length; i++) {
-            if (keccak256(abi.encodePacked(name)) == keccak256(abi.encodePacked(votedNames[i]))) {
-                has = true;
-                break;
-            }
-        }
-
-        if (!has) {
+        if(!votingCountMap[name].exists){
+            VotedCount memory vr=VotedCount({ exists:true,count:1});
+            votingCountMap[name]=vr;
             votedNames.push(name);
+        }else{
+            votingCountMap[name].count+=1;
         }
     }
 
@@ -49,7 +43,8 @@ contract Voting {
      * @return 候选人的得票数
      */
     function getVotes(string calldata name) public view returns (uint) {
-        return votingCountMap[name];
+        VotedCount storage votedCount= votingCountMap[name];
+        return votedCount.count;
     }
 
     /**
@@ -71,15 +66,9 @@ contract Voting {
 
         // 清空候选人列表
         delete votedNames;
-        // for (uint i = 0; i < namesLength; i++) {
-        //     votedNames.pop();
-        // }
-
+    
       
         // 清空投票人地址列表
         delete votingAddrs;
-        // for (uint i = 0; i < addrsLength; i++) {
-        //     votingAddrs.pop();
-        // }
     }
 }
